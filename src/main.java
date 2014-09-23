15508de134c43d7b62ecab3b0fc33569ce4c0788
Mel <mgalvan@D201-2.Tertiaire.loges>
2014-09-23 11:02:35 +0200

    Modif num 3
    Commentaire d'une ligne d'ajout de ville

--- src/main.java
+++ src/main.java
@@ -9,7 +9,7 @@ public class main {
 
 		Pays pays1 = new Pays("France", "Français", "Euro", new Capitale("Paris", 2249975));
 		pays1.ajouterVille(new Ville("Evry", 52184));
-		pays1.ajouterVille(new Ville("Soisy", 6992));
+		//pays1.ajouterVille(new Ville("Soisy", 6992));
 		//System.out.println(pays1.toString());
 		
 		
